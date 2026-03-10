0a0230d952746acdad65e5686e32ee04eec511d5
tomas-citterio <tomcitterio08@gmail.com>
2026-03-10 12:46:26 -0300

    Update exercise_time.py

--- exercise_time.py
+++ exercise_time.py
@@ -8,3 +8,11 @@ def time():
     3. Segundos restantes
     """
     total_segundos = 3665
+    horas_completas = total_segundos // 3600
+    int(horas_completas)
+    minutos_completas = total_segundos % 3600 // 60
+    int(minutos_completas)
+    segundos_restantes = total_segundos - (3600 + 60)
+    print(horas_completas)
+    print(minutos_completas)
+    print(segundos_restantes)
