49fdc4402fd48b2632b7e8a25e2b5790b11abc63
tomas-citterio <tomcitterio08@gmail.com>
2026-03-16 18:21:34 -0300

    Update exercise_circle.py

--- exercise_circle.py
+++ exercise_circle.py
@@ -1,6 +1,3 @@
-from math import pi
-
-
 def circle():
     """
     Ejercicio 6 - Geometría de Círculo
@@ -10,3 +7,7 @@ def circle():
     2. La circunferencia (2 × π × radio)
     """
     radio = 5
+    area = pi * (radio ** 2)
+    circunferencia = (2 * pi * radio)
+    print("Area: ", area)
+    print("Circunferencia: ", circunferencia)
