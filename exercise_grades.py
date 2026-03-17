1150f07a58480339cb412477df0efea61733f00c
tomas-citterio <tomcitterio08@gmail.com>
2026-03-17 10:16:32 -0300

    Update exercise_grades.py

--- exercise_grades.py
+++ exercise_grades.py
@@ -11,11 +11,11 @@ def grades():
     nota1 = 8
     nota2 = 7
     nota3 = 9
-    promedio = (nota1 + nota2 + nota3) / 3
+    promedio = (nota1 + nota2 + nota3)/3
     nota_maxima = max(nota1, nota2, nota3)
     nota_minima = min(nota1, nota2, nota3)
-    puntos_faltantes = 10 - promedio
+    puntos_faltan = 10 - promedio
     print(promedio)
-    print(nota_minima)
     print(nota_maxima)
-    print(puntos_faltantes)
+    print(nota_minima)
+    print(puntos_faltan)
